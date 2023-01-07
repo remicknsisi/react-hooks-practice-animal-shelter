42c347dc5e4c2da21da270d4bfccbbc111685985
Sienna <remicknsisi@gmail.com>
2023-01-07 12:46:08 -0500

    commit tree eb399ce83f631c82f53ca914ced4c725a12bb9b3

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,44 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function handleChangeType(newType){
+    setFilters({ type: newType })
+  }
+
+  function handleFindPets(){
+    if (filters.type === "all"){
+      fetch('http://localhost:3001/pets')
+      .then(res => res.json())
+      .then(petData => setPets(petData))}
+    else if (filters.type === "cat"){
+      fetch('http://localhost:3001/pets?type=cat')
+      .then(res => res.json())
+      .then(petData => setPets(petData))}
+    else if (filters.type === "dog"){
+      fetch('http://localhost:3001/pets?type=dog')
+      .then(res => res.json())
+      .then(petData => setPets(petData))}
+    else if (filters.type === "micropig"){
+      fetch('http://localhost:3001/pets?type=micropig')
+      .then(res => res.json())
+      .then(petData => setPets(petData))}
+    }
+
+  function handleAdopt(id){
+    fetch(`http://localhost:3001/pets/${id}`, {
+      method: 'PATCH',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        "isAdopted": true
+      })
+  })
+    .then(res => res.json())
+    .then(updatedPet => {
+      const newPets = pets.filter(pet => pet !== updatedPet)
+      setPets([...newPets, updatedPet])
+    })
+    }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +53,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onFindPetsClick={handleFindPets} onChangeType={handleChangeType}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdopt}/>
           </div>
         </div>
       </div>
